fd0d200e2911975aa2a70095facc6e7416dfc45e
Muhammad Rafi Fahrezi <rafi05fahrezi@gmail.com>
2023-06-21 11:19:59 +0700

    Add files via upload

---
+++ laravel10-api/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+//import Model "Post"
+use App\Models\Post;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//import Resource "PostResource"
+use App\Http\Resources\PostResource;
+
+//import Facade "Storage"
+use Illuminate\Support\Facades\Storage;
+
+//import Facade "Validator"
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        //get all posts
+        $posts = Post::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    /**
+     * store
+     * 
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(),[
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'nama'     => 'required',
+            'asal'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'nama'  => $request->nama,
+            'asal'  => $request->asal,
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+
+    /**
+     * show
+     * 
+     * @param  mixed $post
+     * @return void
+     */
+
+    public function show($id) 
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //return single post as a resource
+        return new PostResource(true, 'Detail Data Post!', $post);
+    }
+
+    /**
+     * update
+     * 
+     * @param  mixed $request
+     * @param  mixed $posts
+     * @return void
+     */
+
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama'   => 'required',
+            'asal'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //check if image not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/'.basename($post->image));
+
+            //update post with new image
+            $post->update([
+                'image'   => $image->hashName(),
+                'nama'    => $request->nama,
+                'asal'    => $request->asal,
+            ]);
+
+        } else {
+            
+            //update post without image
+            $post->update([
+                'nama'    => $request->nama,
+                'asal'    => $request->asal,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+    }
+
+    /**
+     * destroy
+     * 
+     * @param  mixed $post
+     * @return void
+     */
+    public function destroy($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //delete image
+        Storage::delete('public/posts/'.basename($post->image));
+
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
+
+    }
+}
